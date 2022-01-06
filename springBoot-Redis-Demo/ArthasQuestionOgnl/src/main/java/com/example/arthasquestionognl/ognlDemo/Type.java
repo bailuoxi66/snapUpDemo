67e326eff4b78c17d065004774207a72ed905bfb
luoyu <liangzhengtao.lzt@lazada.com>
2022-01-06 17:01:54 +0800

    feat: 新增Arths Ognl 测试验证

---
+++ springBoot-Redis-Demo/ArthasQuestionOgnl/src/main/java/com/example/arthasquestionognl/ognlDemo/Type.java
@@ -0,0 +1,5 @@
+package com.example.arthasquestionognl.ognlDemo;
+
+public enum Type {
+    RUN, STOP;
+}
---
+++ springBoot-Redis-Demo/ArthasQuestionOgnl/src/test/java/com/example/arthasquestionognl/ArthasQuestionOgnlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arthasquestionognl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthasQuestionOgnlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
